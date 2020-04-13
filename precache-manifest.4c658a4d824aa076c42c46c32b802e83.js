c08ab8dc3afc438fd03b8a3b2e107342ff85cc06
prehooo <kamilnatonek@gmail.com>
2020-04-13 21:11:16 +0200

    Updates

---
+++ precache-manifest.4c658a4d824aa076c42c46c32b802e83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccad1d637efbc34c9310dbfb82f4f25d",
+    "url": "/Barshed/index.html"
+  },
+  {
+    "revision": "955e182ddce0c7204156",
+    "url": "/Barshed/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "8a8d325199ef806873da",
+    "url": "/Barshed/static/css/main.94269ccb.chunk.css"
+  },
+  {
+    "revision": "955e182ddce0c7204156",
+    "url": "/Barshed/static/js/2.d639447a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Barshed/static/js/2.d639447a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8d325199ef806873da",
+    "url": "/Barshed/static/js/main.9b74f2fd.chunk.js"
+  },
+  {
+    "revision": "3d9b7f6c30d6fdd6bea7",
+    "url": "/Barshed/static/js/runtime-main.4746268b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Barshed/static/media/logo.5d5d9eef.svg"
+  }
+]);
